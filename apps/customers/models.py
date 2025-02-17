b67cd2a1566ab5b8a4a2bf89aa20b3cc4cf084a6
josejuniorpg <yunipgx@protonmail.com>
2025-02-16 23:18:07 -0600

    Modify Customer Model.

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 
+from django.db import models
+from model_utils.models import TimeStampedModel
+
 
 class Customer(TimeStampedModel):
     """
-    Represents a customer.
+    Represents a customer belonging to a specific restaurant.
     """
+    restaurant = models.ForeignKey(
+        'restaurants.Restaurant',
+        on_delete=models.CASCADE,
+        related_name="customers",
+        verbose_name="Restaurant"
+    )
     name = models.CharField(max_length=255, verbose_name="Customer Name")
     email = models.EmailField(unique=True, null=True, blank=True, verbose_name="Email")
 
     def __str__(self):
-        return self.name if self.name else "Anonymous Customer"
+        return f"{self.name} ({self.restaurant.name})"
